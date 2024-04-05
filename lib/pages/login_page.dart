56e2a17d4a4fd7bfc09f120eb659e16858d8b804
Wahtu Astrawan <chatt699@gmail.com>
2024-04-06 00:28:15 +0800

    feat: adding login page UI

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:auth_app/components/textfield.dart';
+import 'package:auth_app/components/button.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const SizedBox(height: 50),
+            const Icon(
+              Icons.beach_access,
+              size: 110,
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Halaman Login',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            const SizedBox(height: 40),
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Username',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Lupa Password?',
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyButton(
+              onTap: signUserIn,
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Belum punya akun?',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(width: 4),
+                const Text(
+                  'Daftar sekarang',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
