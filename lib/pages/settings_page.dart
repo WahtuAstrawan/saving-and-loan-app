67d52be550221f76fed398db650ab06a5d32c0ed
Wahtu Astrawan <chatt699@gmail.com>
2024-04-06 11:55:47 +0800

    feat: adding settings page

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Text(
+          "Halaman Setting",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
