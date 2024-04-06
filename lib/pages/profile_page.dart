67b336e15d855072c379d4715ad299c3716d3eec
Wahtu Astrawan <chatt699@gmail.com>
2024-04-06 11:55:35 +0800

    feat: adding profile page

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Text(
+          "Halaman Profil",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
