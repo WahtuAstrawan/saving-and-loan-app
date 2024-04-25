4c411f95de3639007f43be876762b9c10fb328a6
Wahtu Astrawan <chatt699@gmail.com>
2024-04-25 16:08:00 +0800

    feat: adding api connection to register user

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,12 +1,94 @@
 import "package:auth_app/components/button.dart";
+import "package:auth_app/components/show_alert_dialog.dart";
 import "package:auth_app/components/textfield.dart";
+import 'package:email_validator/email_validator.dart';
 import "package:flutter/material.dart";
+import "package:dio/dio.dart";
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class RegisterPage extends StatefulWidget {
+  RegisterPage({super.key});
 
-  void signUpUser(BuildContext context) {
-    Navigator.pushNamed(context, '/login');
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final dio = Dio();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  void signUpUser(BuildContext context) async {
+    if (nameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        confirmPasswordController.text.isEmpty) {
+      showAlertDialog(context, "Error", "Harap mengisi semua kolom");
+      return;
+    }
+
+    final String email = emailController.text;
+
+    if (!EmailValidator.validate(email)) {
+      showAlertDialog(
+          context, "Error", "Format email yang Anda masukkan salah");
+      return;
+    }
+
+    if (passwordController.text != confirmPasswordController.text) {
+      showAlertDialog(
+          context, "Error", "Password yang Anda masukkan tidak sama");
+      return;
+    }
+
+    if (passwordController.text.length < 6 ||
+        confirmPasswordController.text.length < 6) {
+      showAlertDialog(context, "Error",
+          "Password yang Anda masukkan tidak boleh kurang dari 6 karakter");
+      return;
+    }
+
+    try {
+      final response =
+          await dio.post('https://mobileapis.manpits.xyz/api/register', data: {
+        'name': nameController.text,
+        'email': emailController.text,
+        'password': passwordController.text
+      });
+
+      if (!response.data['success']) {
+        showAlertDialog(context, "Error",
+            "Terjadi kesalahan saat mendaftar akun, coba ulang");
+        return;
+      }
+
+      showAlertDialog(context, "Success", "Akun Anda berhasil terdaftar");
+
+      nameController.clear();
+      emailController.clear();
+      passwordController.clear();
+      confirmPasswordController.clear();
+      return;
+    } on DioException catch (e) {
+      if (e.response != null && e.response!.statusCode! < 500) {
+        String errorMessage = "Internal Server Error";
+        if (e.response!.data != null && e.response!.data['message'] != null) {
+          var message = e.response!.data['message'];
+          if (message is Map && message.isNotEmpty) {
+            var firstMessage = message.values.first;
+            if (firstMessage is List && firstMessage.isNotEmpty) {
+              errorMessage = firstMessage[0];
+            }
+          }
+        }
+        showAlertDialog(context, "Error", errorMessage);
+      } else {
+        showAlertDialog(context, "Error", "Internal Server Error");
+      }
+      return;
+    }
   }
 
   @override
@@ -15,50 +97,73 @@ class RegisterPage extends StatelessWidget {
       backgroundColor: Colors.grey[100],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              const Icon(Icons.beach_access, size: 110),
-              const SizedBox(height: 40),
-              const Text('Halaman Register',
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const Icon(Icons.beach_access, size: 110),
+                const SizedBox(height: 40),
+                const Text(
+                  'Halaman Register',
                   style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-              const SizedBox(height: 40),
-              const MyTextField(hintText: 'Nama Lengkap', obscureText: false),
-              const SizedBox(height: 10),
-              const MyTextField(hintText: 'Username', obscureText: false),
-              const SizedBox(height: 10),
-              const MyTextField(hintText: 'Password', obscureText: true),
-              const SizedBox(height: 10),
-              const MyTextField(
-                  hintText: 'Konfirmasi Password', obscureText: true),
-              const SizedBox(height: 25),
-              MyButton(onTap: () => signUpUser(context), buttonText: 'Daftar'),
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Sudah punya akun?',
-                    style: TextStyle(color: Colors.grey[700]),
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(width: 4),
-                  GestureDetector(
-                    child: const Text(
-                      'Login sekarang',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ),
+                const SizedBox(height: 40),
+                MyTextField(
+                  controller: nameController,
+                  hintText: 'Nama Lengkap',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'Konfirmasi Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+                MyButton(
+                  onTap: () => signUpUser(context),
+                  buttonText: 'Daftar',
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Sudah punya akun?',
+                      style: TextStyle(color: Colors.grey[700]),
                     ),
-                    onTap: () => signUpUser(context),
-                  ),
-                ],
-              )
-            ],
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      child: const Text(
+                        'Login sekarang',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () => Navigator.pushNamed(context, '/login'),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
