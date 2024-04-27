c5fb4dd8f2f97cd337532b08249516ffb1a34b61
Wahtu Astrawan <chatt699@gmail.com>
2024-04-27 11:40:34 +0800

    fix: remove const for safety

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:get_storage/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
