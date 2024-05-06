83a852e9dddf727ca4bb0e0db95796e4a0b62634
Wahtu Astrawan <chatt699@gmail.com>
2024-05-06 15:18:35 +0800

    chore: convert to stateful widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: const Center(
         child: Text(
-          "Halaman Home",
+          "Halaman Home List Anggota",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
